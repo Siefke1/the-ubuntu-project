369db7defe9b731f932c01783196036bf1113d71
León Langhoff <leon@datora.de>
2025-10-22 16:33:01 +0200

    adjust landing page text

--- src/App.tsx
+++ src/App.tsx
@@ -147,10 +147,10 @@ function AppContent() {
             Ever wanted to own a company? Our idea is simple.{" "}
             <Box component="br" sx={{ display: { xs: "none", md: "block" } }} />{" "}
             We founded an online company and for now we provide this platform.
-            If you sign up on our platform, you become an owner of the company.
-            And whatever income our shared little business generates, everyone
-            will get their equal share. You, us and everyone else. No
-            smallprint, no catches.
+            If you sign up on our platform, you become an equal shareholder in
+            our company, meaning that whatever revenue our shared little business
+            generates, everyone will get their equal share. You, us and everyone
+            else. No smallprint, no catches.
           </Typography>
         </motion.div>
 
