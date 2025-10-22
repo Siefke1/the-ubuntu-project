af23b629395d7dcb298759894693c97ffa140a76
León Langhoff <leon@datora.de>
2025-10-22 21:03:51 +0200

    rm old files

--- src/context/LanguageContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-export interface LanguageContextType {
-  language: string;
-  setLanguage: (language: string) => void;
-}
-
-export const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
